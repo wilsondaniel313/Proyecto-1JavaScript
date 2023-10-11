1c0543804faee812762f0538d65357eb8e793ed3
wilsonosma <110642281+wilsondaniel313@users.noreply.github.com>
2023-10-10 20:44:32 -0500

    Add files via upload

---
+++ Calculadora/Script.js
@@ -0,0 +1,37 @@
+const pantalla = document.querySelector(".pantalla");
+const botones = document.querySelectorAll(".boton");
+botones.forEach(boton => {
+    boton.addEventListener("click",() => {
+       const botonOn = boton.textContent;
+
+        if(boton.id === "borrar" ){
+            if(pantalla.textContent.length === 1 || pantalla.textContent === "¡ Error !"){
+                pantalla.textContent ="0";
+            }else{
+                pantalla.textContent=pantalla.textContent.slice(0,-1);
+            }
+            return;
+        }
+        if(boton.id ==="igual"){
+        try{
+            pantalla.textContent = eval(pantalla.textContent);
+        }catch{
+            pantalla.textContent="¡ Error !";
+        }
+        return;
+            
+        }if(pantalla.textContent === "0" || pantalla.textContent ==="¡ Error ! "){
+            pantalla.textContent = botonOn;
+        }else{
+            pantalla.textContent +=botonOn
+        }
+        if (boton.id ==="limpiar"){
+            pantalla.textContent= "0";
+            return;
+        }
+    
+   
+    })
+
+    
+});
